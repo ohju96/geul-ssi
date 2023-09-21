5f2af8b0cacd0539eccc14235873e5efcfbcb802
ohju96 <ohju96@naver.com>
2023-09-21 22:10:05 +0900

    feat[#2, #3] : toml, git ignore, database 설정

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"geulSsi/config"
+	"geulSsi/config/db"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	app := gin.Default()
 
+	Init(app)
+
 	app.Run()
+}
+
+func Init(app *gin.Engine) {
+	// toml
+	toml := config.InitToml("./config/config.toml")
+
+	// db
+	db.InitMySQL(&toml)
 }
