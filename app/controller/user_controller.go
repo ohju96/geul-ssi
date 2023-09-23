9b950d80e7fe9e6c9ee5163b055fcc28ba1da8db
ohju96 <ohju96@naver.com>
2023-09-23 16:07:02 +0900

    feat[#3] : 회원가입 스웨거 등록

--- app/controller/user_controller.go
+++ app/controller/user_controller.go
@@ -8,13 +8,24 @@ import (
 )
 
 type UserController interface {
-	CreateUser(g *gin.Context)
+	CreateUser(g *gin.Context) // 회원가입
 }
 
 type userControllerImpl struct {
 	userService service.UserService
 }
 
+// CreateUser godoc
+// @Summary 회원가입
+// @Description 회원가입을 하면 SecretCode를 발급합니다. SecretCode로 비밀번호 변경이나 회원 찾기에 사용되나 재발급은 되지 않으니 보관 안내가 필요합니다.
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param userDto.CreateUserRequest body userDto.CreateUserRequest true "회원가입 요청 객체"
+// @Success 200 {object} userDto.CreateUserResponse
+// @Failure 400 {object} custom.Fail400GetResponse
+// @Failure 500 {object} custom.Fail500GetResponse
+// @Router /api/v1/users [post]
 func (c userControllerImpl) CreateUser(g *gin.Context) {
 
 	var createUserRequest userDto.CreateUserRequest
--- app/dto/custom/custom_dto.go
+++ app/dto/custom/custom_dto.go
@@ -15,3 +15,15 @@ func NewSuccess() Response {
 		Message:    "success",
 	}
 }
+
+type Fail400GetResponse struct {
+	StatusCode int    `json:"status_code" example:"400"`
+	Code       int    `json:"code" example:"1001"`
+	Message    string `json:"message" example:"bad request"`
+}
+
+type Fail500GetResponse struct {
+	StatusCode int    `json:"status_code" example:"500"`
+	Code       int    `json:"code" example:"1003"`
+	Message    string `json:"message" example:"internal server error"`
+}
