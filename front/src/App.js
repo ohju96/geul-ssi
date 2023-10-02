ef573122c7b1c8b291d435063e964bc9b0eed495
Jaenk-99 <jaehy0429@gmail.com>
2023-10-02 15:07:42 +0900

    set[#21]:프론트 셋팅

---
+++ front/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h2 className="font-dos">Hello, React!</h2>
+      <h2 className="font-ram">Hello, React!</h2>
+    </div>
+  );
+}
+
+export default App;
